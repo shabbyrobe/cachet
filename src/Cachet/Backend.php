c682db78c35b9607953209e492c13416a3c3be1b
shabbyrobe <code@shabbyrobe.org>
2013-09-12 02:01:46 +1000

    better docs

--- src/Cachet/Backend.php
+++ src/Cachet/Backend.php
@@ -7,7 +7,9 @@ interface Backend
      * Retrieves an item from the backend. This does not check
      * whether the item has expired - this is up to the Cachet\Cache.
      * This must return null if the item is not found
-     * @param string key to look up
+     *
+     * @param string $cacheId
+     * @param string $key
      * @return Cachet\Item|null
      */
     function get($cacheId, $key);
@@ -21,11 +23,15 @@ interface Backend
      * Delete value from the cache that corresponds to the key.
      * This method is not required to return any value indicating
      * success or failure.
+     *
+     * @param string $cacheId
+     * @param string $key
      * @return void
      */
     function delete($cacheId, $key);
     
     /**
+     * @param string $cacheId
      * @return void
      */
     function flush($cacheId);
