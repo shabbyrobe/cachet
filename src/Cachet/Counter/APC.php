e97b253a4d24a48934e2024af7dccda3e69726bb
shabbyrobe <code@k3jw.com>
2013-10-29 21:00:41 +1100

    APC counter APC test fix

--- src/Cachet/Counter/APC.php
+++ src/Cachet/Counter/APC.php
@@ -4,39 +4,54 @@ namespace Cachet\Counter;
 class APC implements \Cachet\Counter
 {
     public $prefix;
+    public $counterTTL;
 
     function set($key, $value)
     {
         if (!is_int($value))
             throw new \InvalidArgumentException();
-        apc_store($key, $value);
+
+        $formattedKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
+        if (!apc_store($formattedKey, $value, $this->counterTTL)) {
+            throw new \UnexpectedValueException("APC could not set the value at $formattedKey");
+        }
     }
 
     function value($key)
     {
-        $fullKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
-        $value = apc_fetch($fullKey);
-        if (!is_int($value))
-            throw new \UnexpectedValueException();
+        $formattedKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
+        $value = apc_fetch($formattedKey);
+        if (!is_numeric($value) && !is_bool($value) && $value !== null) {
+            $type = \Cachet\Helper::getType($value);
+            throw new \UnexpectedValueException(
+                "APC counter expected numeric value, found $type at key $key"
+            );
+        }
         return $value ?: 0;
     }
 
     function increment($key, $by=1)
     {
-        $fullKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
-        $value = apc_inc($fullKey, $by, $success);
-        if (!$success)
-            throw new \UnexpectedValueException("APC could not increment the key at $fullKey");
+        $formattedKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
+        $value = apc_inc($formattedKey, $by, $success);
+        if (!$success) {
+            $success = apc_store($formattedKey, $by, $this->counterTTL);
+            if (!$success)
+                throw new \UnexpectedValueException("APC could not increment the key at $formattedKey");
+        }
 
         return $value;
     }
 
     function decrement($key, $by=1)
     {
-        $fullKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
-        $value = apc_dec($fullKey, $by, $success);
-        if (!$success)
-            throw new \UnexpectedValueException("APC could not decrement the key at $fullKey");
+        $formattedKey = \Cachet\Helper::formatKey([$this->prefix, 'counter', $key]);
+        $value = apc_dec($formattedKey, $by, $success);
+        if (!$success) {
+            $success = apc_store($formattedKey, -$by);
+            if (!$success)
+                throw new \UnexpectedValueException("APC could not increment the key at $formattedKey");
+        }
 
         return $value;
     }
--- test/lib/Counter/APCTest.php
+++ test/lib/Counter/APCTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Cachet\Test\Backend;
+namespace Cachet\Test\Counter;
 
 if (!extension_loaded('apc') || !extension_loaded('apcu')) {
     skip_test(__NAMESPACE__, "APCTest", "Neither apc nor apcu loaded");
@@ -8,12 +8,11 @@ elseif (ini_get('apc.enable_cli') != 1) {
     skip_test(__NAMESPACE__, "APCTest", "apc.enable_cli must be set");
 }
 else {
-    class APCTest extends \Cachet\Test\BackendTestCase
+    class APCTest extends \Cachet\Test\CounterTestCase
     {
-        public function getBackend()
+        public function getCounter()
         {
-            $backend = new \Cachet\Backend\APC();
-            return $backend;
+            return new \Cachet\Counter\APC();
         }
     }
 }
