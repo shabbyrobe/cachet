3b3e0bef1423a6812e9a62ae9836d7bbd763f1a7
bl <bl@i3.vb>
2013-12-08 18:48:52 +1100

    fetching iterator now accepts raw array

--- src/Cachet/Backend/Iterator/Fetching.php
+++ src/Cachet/Backend/Iterator/Fetching.php
@@ -7,6 +7,12 @@ class Fetching extends \IteratorIterator
 {
     function __construct($cacheId, $keyIterator, Backend $backend)
     {
+        if (is_array($keyIterator))
+            $keyIterator = new \ArrayIterator($keyIterator);
+    
+        if (!$keyIterator instanceof \Iterator)
+            throw new \InvalidArgumentException();
+
         parent::__construct($keyIterator);
         $this->cacheId = $cacheId;
         $this->backend = $backend;
