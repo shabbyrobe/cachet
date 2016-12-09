483e51e039b5264bac1a9e6d0d70010fa0317d46
shabbyrobe <code@shabbyrobe.org>
2016-12-09 13:48:36 +1100

    EVEN MORE apc.

--- test/web/sessionhandler/config.php
+++ test/web/sessionhandler/config.php
@@ -2,6 +2,12 @@
 require __DIR__.'/../../../src/Cachet.php';
 Cachet::register();
 
-$backend = new Cachet\Backend\APC();
+if (extension_loaded('apcu')) {
+    $backend = new Cachet\Backend\APCU();
+} else if (extension_loaded('apc')) {
+    $backend = new Cachet\Backend\APC();
+} else {
+    throw new \Exception('No supported backend available for session testing');
+}
 $cache = new Cachet\Cache('session', $backend);
 Cachet\SessionHandler::register($cache);
