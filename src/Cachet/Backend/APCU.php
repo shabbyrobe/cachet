7afd8e77a870d772fb55c65c426c220b49c3bf93
shabbyrobe <code@shabbyrobe.org>
2016-12-09 13:31:16 +1100

    ruinous BC bugs from apcu. get it together.

--- src/Cachet/Backend/APCU.php
+++ src/Cachet/Backend/APCU.php
@@ -5,6 +5,13 @@ use Cachet\Dependency;
 use Cachet\Backend;
 use Cachet\Item;
 
+// as at 201612, there is some weirdness around these constants. the latest
+// version of APCU doesn't appear to have the appropriate APCU_* constants
+// defined, even though the documentation references them.
+if (!defined('APCU_ITER_VALUE') && defined('APC_ITER_VALUE')) {
+    define('APCU_ITER_VALUE', APC_ITER_VALUE);
+}
+
 /**
  * Starting with PHP 7.0, apcu removed the apc_* functions by default,
  * moving them into a separate extension called apc_bc. 
